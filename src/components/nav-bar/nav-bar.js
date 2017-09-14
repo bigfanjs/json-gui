23cf53c438a04d1119459130a75013b0684be3ee
Adel Jojo <bigfanjs@gmail.com>
2017-09-14 12:33:31 +0100

    feat: creates a nav bar placeholder component

---
+++ src/components/nav-bar/nav-bar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './nav-bar.css';
+
+export default function () {
+  return (
+    <div className="navbar">
+      Nav Bar Placeholder Component.
+    </div>
+  );
+}
